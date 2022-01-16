cc470dc24ec2bae0e37db26f87105a6f9702e2c5
Diplomacy <baptiste.ch@live.fr>
2022-01-16 01:44:39 +0100

    Ajoutez des fichiers projet.

---
+++ ShadowWontSleep/Imports.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ShadowWontSleep
+{
+    public static class Imports
+    {
+        [Flags]
+        public enum MouseEventFlags : uint
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010,
+            WHEEL = 0x00000800,
+            XDOWN = 0x00000080,
+            XUP = 0x00000100
+        }
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(MouseEventFlags dwFlags, int dx, int dy, uint dwData, ulong dwExtraInfo);
+    }
+}
---
+++ ShadowWontSleep/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace ShadowWontSleep
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool wasPreviousMoveLeft = true;
+
+            Console.WriteLine("No Shadow, you won't sleep while im awake");
+
+            while (true)
+            {
+                int moveXY = wasPreviousMoveLeft ? 10 : -10;
+                Imports.mouse_event(Imports.MouseEventFlags.MOVE, moveXY, moveXY, 0, 0);
+                wasPreviousMoveLeft = !wasPreviousMoveLeft;
+                Console.WriteLine("Wake Up!");
+                Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
